87fdd176c23696758afb261e124a18b8647c4372
Georg Schwarz <schwarz@group.riehle.org>
2025-03-28 19:55:55 +0100

    Implement main script to demo the boss generation

---
+++ ask-ai.py
@@ -0,0 +1,19 @@
+import dotenv
+from boss_battle.boss_generation.open_router_client import OpenRouterClient
+
+dotenv.load_dotenv()
+
+def main():
+    client = OpenRouterClient("deepseek/deepseek-r1:free")    
+    prompt = "I want an orc as a boss."
+    
+    try:
+        structured_output = client.get_structured_output(prompt)
+        
+        print("Your boss:")
+        print (structured_output.model_dump_json(indent=2))
+    except Exception as e:
+        print(f"Error: {str(e)}")
+
+if __name__ == "__main__":
+    main()
